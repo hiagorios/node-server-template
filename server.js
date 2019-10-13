c878850e356fd90dac0aac06085cce7d82c6ba1c
Hiago Rios <hiagoriosc@gmail.com>
2019-10-13 01:12:59 -0300

    Trying to connect to mongodb

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+// Initializing the app
 const app = express();
 
+// Initializing DB
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+);
+
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
+// Creating the first route
 app.get('/', (req, res) => {
-    res.send('Aloww desgraça')
+    Product.create({
+        title: 'React Native',
+        description: 'Build native apps with React',
+        url: 'http://github.com/facebook/react-native'
+    });
+    return res.send('Aloww desgraça');
 })
 
-app.listen(3002);
+app.listen(3001);
